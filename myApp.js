c7bc2c5a056350c18460be70be4c2f3194af3dba
Mani Movassagh <mani.mgh@gmail.com>
2022-06-26 02:30:53 +0200

    test

--- myApp.js
+++ myApp.js
@@ -10,18 +10,21 @@ app.get('/', (req, res) => {
 
  app.use("/public",express.static(__dirname + "/public"));
 
+
+ function convertToUppercase(){
+  if(process.env.MESSAGE_STYLE == 'uppercase'){
+    return "Hello json".toUpperCase();
+  } else {
+    return "Hello json";
+  }
+  return 
+ }
  app.get('/json', (req, res) => {
 
-  if(process.env.MESSAGE_STYLE == 'uppercase'){
-
-    res.json({"message": "HELLO JSON"});
-
-  } 
-  // else{
-
-  //   res.json({"message": "Hello json"});
-  // }
+      res.json(convertToUppercase());
+    
 })
+console.log(process.env.MESSAGE_STYLE == 'uppercase');
 
 
 
